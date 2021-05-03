a905c9cee84a53cbb36a93c0e7eb1370696183e0
C-YooJin <ioi_3312@naver.com>
2021-05-03 22:55:23 +0900

    Initial commit

---
+++ src/main/java/com/deepjin/lite/LiteApplication.java
@@ -0,0 +1,13 @@
+package com.deepjin.lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deepjin/lite/LiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepjin.lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
