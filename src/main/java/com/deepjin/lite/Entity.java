a19186da61fd69090c8bc081a372384ce15a0bea
C-YooJin <ioi_3312@naver.com>
2021-05-04 13:54:43 +0900

    Add Entity class and Fix bugs

---
+++ src/main/java/com/deepjin/lite/Entity.java
@@ -0,0 +1,41 @@
+package com.deepjin.lite;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@javax.persistence.Entity
+public class Entity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String message;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO ENTITY(name, message) values("Kelly", "This is Kelly");
+INSERT INTO ENTITY(name, message) values("Maru", "I'm her dog");
